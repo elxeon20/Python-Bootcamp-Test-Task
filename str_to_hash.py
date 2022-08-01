19eb06f1d3e73defa3b838d3d4f94c4ece2059e0
elxeon20 <99205252+elxeon20@users.noreply.github.com>
2022-08-01 20:23:59 +0300

    Update str_to_hash.py

--- str_to_hash.py
+++ str_to_hash.py
@@ -1,17 +1,5 @@
-
-n = int(input())
-list = []
-for i in range(n):
-    list = [[0 for x in range(2)] for y in range(n)]
-for i in range(n):
-    list[i] = input().split()
-list2 = []
-for i in range(int(input())):
-    list2.append(input())
-for i in list2:
-    for d in range(n):
-        if i in list[d]:
-            print(list[d][0])
-
-                   
-            
+import hashlib
+s = "Python Bootcamp"
+hash_object = hashlib.sha256(b'${s}')
+hex_dig = hash_object.hexdigest()
+print(hex_dig)
